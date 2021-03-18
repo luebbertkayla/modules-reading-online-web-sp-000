db13b046956c2a4f33446b0ffe7a9dde25e1aea1
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-18 23:04:05 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,6 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-  include Dance
-  extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
  
